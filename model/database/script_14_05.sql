8ccbfaa567eb4a96bd75844b76ae6da171794564
Daniele-SS <dev.daniele@outlook.com>
2026-05-14 08:19:07 -0300

    feat: adiciona novos comandos de formatação de horários e datas

--- model/database/script_14_05.sql
+++ model/database/script_14_05.sql
@@ -14,4 +14,24 @@ select curtime() as hora_atual;
 select current_timestamp() as data_hora_atual;
 
 
+## COMANDOS PARA FORMATAÇÃO DE DATA E HORA ##
 
+select time_format(curtime(), '%H') as hora; #Hora de 00 ... 23
+select time_format('21:50', '%h') as hora; #Hora de 0 ... 12
+
+select time_format(curtime(), '%i') as minuto;
+select time_format(curtime(), '%s') as segundo;
+
+select time_format(curtime(), '%H:%i') as hora_minuto;
+select time_format(curtime(), '%H:%s') as hora_segundo;
+select time_format(curtime(), '%H:%i:%s') as hora_minuto_segundo;
+
+select time_format(curtime(), '%r') as hora_formatada; #Retorna o horário em AM ou PM
+
+select time_format(curtime(), '%T') as hora_formatada; #Formato 24horas
+
+
+# FUNÇÕES DE HORAS, MINUTOS E SEGUNDOS ->
+select hour(current_time()) as hora;
+select minute(current_time()) as minuto;
+select second(current_time()) as segundo;
